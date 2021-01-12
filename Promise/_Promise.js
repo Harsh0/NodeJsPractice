d70d643afccc23dfbf2caa2ae6efa03d3a36a48e
Harsh Singhal <harsh.singhal040@gmail.com>
2021-01-12 18:48:34 +0530

    updated _Promise using class

--- Promise/_Promise.js
+++ Promise/_Promise.js
@@ -1,87 +1,58 @@
-const _Promise = function(cb){
-	let a = {}
-	if(!(cb&&{}.toString.call(cb)=='[object Function]')){
-		throw new Error(`Promise resolve ${cb} is not a function`);
-	}
-	let temp = {};
-	temp.functionChain = [];
-	this.then = (successCallback)=>{
-		temp.functionChain.push({type:'then',func:successCallback});
-		return this;
-	};
-	this.catch = (errCallback)=>{
-		temp.functionChain.push({type:'catch',func:errCallback});
-		return this;
-	};
-	this.transferEventListeners = (functionChain)=>{
-		temp.functionChain = functionChain;
-	}
-	temp.resolve = (data)=>{
-		let funcChain = temp.functionChain;
-		for(let i=0;i<funcChain.length;i++){
-			if(funcChain[i].type=='then'){
-				try{
-					let newPromOrData =  funcChain[i].func(data);
-					if(newPromOrData instanceof _Promise){
-						//promise has received
-						//put all other function to newpromise object
-						newPromOrData.transferEventListeners(funcChain.slice(i+1));
-						process.nextTick(()=>{
-							delete this;//remove this promise;
-						})
-						break;
-						//when the resolve or reject of new promise called functions will be fetched from this chain and executed
-					}else{
-						data = newPromOrData;
-						//in next iteration this data will be used to call next then
-					}
-				}catch(err){
-					//call reject
-					process.nextTick(()=>{
-						temp.reject(err);
-					})
-					break;
-				}
-			}
-		}
-	};
-	temp.reject = (err)=>{
-		//find the first catch and execute and run other functions if available after that
-		let funcChain = temp.functionChain;
-		for(let i=0;i<funcChain.length;i++){
-			if(funcChain[i].type=='catch'){
-				try{
-					let newPromOrData = funcChain[i].func(err);
-					if(newPromOrData instanceof _Promise){
-						newPromOrData.transferEventListeners(funcChain.slice(i+1));
-						process.nextTick(()=>{
-							delete this;
-						})
-						break;
-					}else{
-						temp.functionChain = funcChain.slice(i+1);
-						process.nextTick(()=>{
-							temp.resolve(newPromOrData);
-						})
-						break;
-					}
-				}catch(err){
-					//find other catch and execute
-					temp.functionChain = funcChain.slice(i+1);
-					process.nextTick(()=>{
-						temp.reject(err);
-					});
-					break;
-				}
-			}
-		}
-	}
-	process.nextTick(()=>{
-		try{
-			cb(temp.resolve,temp.reject);
-		}catch(err){
-			temp.reject(err);
-		}
-	});
+class _Promise {
+    constructor(cb) {
+        if (!(cb && {}.toString.call(cb) == '[object Function]')) {
+            throw new TypeError(`Promise resolver ${cb} is not a function`);
+        }
+        this.functionChain = [];
+        process.nextTick(() => {
+            try {
+                cb(this.resolve, this.reject);
+            } catch (err) {
+                this.reject(err);
+            }
+        });
+    }
+    resolve = (data) => {
+        const successHandlerIndex = this.functionChain.findIndex((e) => e.type === 'success');
+        if (successHandlerIndex < 0) {
+            return;
+        }
+        this.handle(successHandlerIndex, data);
+    };
+    reject = (error) => {
+        const errorHandlerIndex = this.functionChain.findIndex((e) => e.type === 'error');
+        if (errorHandlerIndex < 0) {
+            return console.log(
+                `UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch().`
+            );
+        }
+        this.handle(errorHandlerIndex, error);
+    };
+    handle(handlerIndex, value) {
+        const handler = this.functionChain[handlerIndex].func;
+        this.functionChain = this.functionChain.slice(handlerIndex + 1);
+        try {
+            const returnedValue = handler(value);
+            if (returnedValue instanceof _Promise) {
+                returnedValue.setFunctionChain(this.functionChain);
+                return;
+            }
+            this.resolve(returnedValue);
+        } catch (error) {
+            this.reject(error);
+        }
+    }
+    then(successCallback) {
+        this.functionChain.push({ type: 'success', func: successCallback });
+        return this;
+    }
+    catch(errorCallback) {
+        this.functionChain.push({ type: 'error', func: errorCallback });
+        return this;
+    }
+    setFunctionChain(functionChain) {
+        this.functionChain = functionChain;
+    }
 }
+
 module.exports = _Promise;
